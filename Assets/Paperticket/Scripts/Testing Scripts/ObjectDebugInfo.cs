c5fe2c6300161bc0475014307774c323dc53a38d
Sean OConnell <pinky@paperticketstudios.com>
2021-02-19 05:47:00 +1100

    V1.1 - small changes to IN05, IN06

---
+++ Assets/Paperticket/Scripts/Testing Scripts/ObjectDebugInfo.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Paperticket;
+using TMPro;
+
+public class ObjectDebugInfo : MonoBehaviour {
+
+    [SerializeField] TextMeshPro tmp = null;
+
+    [Header("CONTROLS")]
+    [Space(5)]
+    [SerializeField] Transform target = null;
+
+    [Header("LIVE VARIABLES")]
+    [Space(5)]
+    public bool Show = true;
+
+
+    private void OnEnable() {
+        if (target == null) {
+            Debug.LogWarning("[ObjectDebugInfo] No target assigned! Disabling.");
+            gameObject.SetActive(false);
+        }
+    }
+
+
+    void Update() {
+        if (Show == true) UpdateText();    
+    }
+
+
+    public void UpdateText() {
+
+
+        tmp.text = "[DEBUG INFO: " + target.name + "]\n " +
+                    "Position = " + target.position.ToString() + " Rotation = " + target.rotation.eulerAngles.ToString() + "\n " +
+                    "Local Position = " + target.localPosition.ToString() + " Rotation = " + target.localRotation.eulerAngles.ToString() + "\n " +
+                    "Parent = " + target.parent.ToString();
+
+
+    }
+
+
+
+}
